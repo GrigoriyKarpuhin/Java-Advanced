2aa9d25d1c1228f9dcb765c73a9887624a7ed176
Oleg Fadeev <ofadeev@solanteq.com>
2024-04-05 15:01:10 +0300

    Checked by an instructor

--- scripts/implJar.sh
+++ scripts/implJar.sh
@@ -4,4 +4,4 @@ cd ../
 javac -cp ../java-advanced-2024/modules/info.kgeorgiy.java.advanced.implementor/ -d scripts java-solutions/info/kgeorgiy/ja/karpukhin/implementor/*.java
 cd scripts
 jar cfm Implementor.jar manifest.mf info/kgeorgiy/ja/karpukhin/implementor/*.class info/kgeorgiy/java/advanced/implementor/*.class
-rm -r info/
+#rm -r info/
